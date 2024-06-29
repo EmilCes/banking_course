878c77e36c06e71b9d287f2056b0d9ce98c4c0e3
Cesar <50472267+EmilCes@users.noreply.github.com>
2024-06-28 23:23:03 -0600

    Implement Plaid Link and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -15,6 +15,7 @@ import { authFormSchema } from "@/lib/utils"
 import { Loader2 } from "lucide-react"
 import { useRouter } from "next/navigation"
 import { signIn, signUp } from "@/lib/actions/user.actions"
+import PlaidLink from "./PlaidLink"
 
 const AuthForm = ({ type }: { type: string }) => {
 
@@ -39,9 +40,22 @@ const AuthForm = ({ type }: { type: string }) => {
         try {
 
             // Sign up with Appwrite & create plain link
-
             if (type === 'sign-up') {
-                const newUser = await signUp(data);
+                
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password
+                };
+
+                const newUser = await signUp(userData);
 
                 setUser(newUser);
             }
@@ -99,9 +113,9 @@ const AuthForm = ({ type }: { type: string }) => {
 
             {user ? (
                 <div className="flex flex-col gap-4">
-                    {/* Plaid Link */}
+                    <PlaidLink user={user} variant="primary" />
                 </div>
-            ) :
+            ): (
                 <>
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -216,7 +230,7 @@ const AuthForm = ({ type }: { type: string }) => {
                         </Link>
                     </footer>
                 </>
-            }
+            )}
 
         </section>
     )
